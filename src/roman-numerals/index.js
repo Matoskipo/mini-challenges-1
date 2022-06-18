f3e816c6a9a789e0ff458e9cfd49a957b4ee9295
Matoskipo <matthew.ataekong@decagon.dev>
2022-06-18 09:44:32 +0100

    my commit

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,44 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+ function romanToDecimal(roman) {
+    const numerals ={
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+    let splitted = roman.toUpperCase().split("");
+    let sum = 0;
+    let i = 0;
+    while(i < splitted.length){
+      if((splitted[i] === "I" && splitted[i+1] === "V") ||
+      (splitted[i] === "I" && splitted[i+1] === "X")||
+      (splitted[i] === "X" && splitted[i+1] === "L")||
+      (splitted[i] === "X" && splitted[i+1] === "C")||
+      (splitted[i] === "C" && splitted[i+1] === "D")||
+      splitted[i] === "C" && splitted[i+1] === "M")
+      {
+         sum+= (numerals[splitted[i+1]] - numerals[splitted[i]]);
+         i+=2;
+      }
+      else{
+        if(numerals[splitted[i]])
+        {
+          sum+= numerals[splitted[i]]
+          i++;
+        }
+      }
+    }
+    return sum;
+   }
 
-module.exports = romanToDecimal;
+
+
+
+   console.log(romanToDecimal(""))
+
+   module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,21 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
-
-module.exports = transpose;
+ function transpose(array) {
+    const matrix =[]
+    for(let i = 0; i < array[0].length; i++)
+    {
+      const eachCol = []
+      for(let j = 0; j < array.length; j++)
+      {
+        eachCol.push(array[j][i])
+      }
+      matrix.push(eachCol);
+    }
+    return matrix;
+  }
+  console.log(transpose([
+    [1, 7, 9],
+        [2, 3, 5],
+  ]))
+  module.exports = transpose;
