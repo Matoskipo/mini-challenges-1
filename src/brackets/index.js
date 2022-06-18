b0c72e01476885af1418f78de93bbfc1d7465277
Matoskipo <matthew.ataekong@decagon.dev>
2022-06-18 09:34:32 +0100

    my commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,32 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let part ={
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  let store = [];
+  for(let i=0; i< str.length; i++)
+  {
+    if(part[str[i]])
+    {
+      store.push(part[str[i]]);
+    }
+    else{
+      let x = store.pop();
+      if(x != store.pop)
+      {
+        return false;
+      }
+
+    }
+  }
+  return store.length === 0;
+}
 
 module.exports = isValid;
+
+
+
